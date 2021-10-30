727d9b98ce0477aede3c254a79d1c01d12189ae0
Hank-ho <chengfeng.ho@gmail.com>
2021-10-30 18:04:04 +0800

    Outputting "Places"

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -12,7 +12,7 @@ export class PlacesService {
       'p1',
       'Manhattan mansion',
       'In the heart of New York City.',
-      'https://www.lonelyplanet.com/articles/top-things-to-do-in-portugal',
+      'https://lp-cms-production.imgix.net/2021-10/Dusseldorf%20%28c%29%20Your%20Little%20Black%20Book_Ashkan%20Mortezapour.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=75&dpr=1',
       149.99
     ),
     new Place
@@ -20,7 +20,7 @@ export class PlacesService {
       'p2',
       'L\'Amour Toujours',
       'A romantic place in Paris',
-      'https://www.lonelyplanet.com/articles/best-day-trips-from-charleston',
+      'https://lp-cms-production.imgix.net/2019-06/GettyImages-562412011_medium.jpg?auto=compress&crop=entropy&fit=crop&format=auto&h=416&w=960',
       149.99
     ),
     new Place
@@ -28,14 +28,14 @@ export class PlacesService {
       'p3',
       'The Foggy Palace',
       'Not your average city trip',
-      'https://www.lonelyplanet.com/articles/germany-north-rhine-westphalia-today',
+      'https://lp-cms-production.imgix.net/2021-10/GettyImages-1145406277.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=75&dpr=1',
       149.99
     ) 
 
   ]
   
   get places() {
-    return [...this.places]
+    return [...this._places]
   }
 
   constructor() { }
